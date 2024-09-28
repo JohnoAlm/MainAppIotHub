a816e484b898d299ccfd24e84c137bdcbbf59464
JohnoAlm <almenoliver02@gmail.com>
2024-09-28 12:28:23 +0200

    Added styling, CommunityToolkit.Maui StatusBarBehavior and platform specific color to Andriod nav bar.

--- MainAppIotHub/Platforms/Android/MainActivity.cs
+++ MainAppIotHub/Platforms/Android/MainActivity.cs
@@ -7,5 +7,18 @@ namespace MainAppIotHub
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetNavigationBarColor();
+        }
+
+        private void SetNavigationBarColor()
+        {
+            if(Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                Window!.SetNavigationBarColor(Android.Graphics.Color.Black);
+            }
+        }
     }
 }
